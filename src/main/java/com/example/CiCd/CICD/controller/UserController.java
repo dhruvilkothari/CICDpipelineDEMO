8a9556b0d7381e9e7c202bcf392b90ac5802edc4
Dhruvil Kothari <kotharidhruvil3@gmail.com>
2025-05-17 18:28:13 +0530

    Added BuildSpec

--- src/main/java/com/example/CiCd/CICD/controller/UserController.java
+++ src/main/java/com/example/CiCd/CICD/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.CiCd.CICD.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ public class UserController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello, World2!";
+    }
+
+    @GetMapping("/{id}")
+    public String hello2(@PathVariable  String id) {
+        return "Hello,"+ id;
     }
 }
